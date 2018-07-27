874f1e94779201c36688f70c0bb300f602a1f728
cuijian <jsflzhong@163.com>
2018-07-27 15:07:15 +0800

    first commit

--- src/main/java/com/michael/springBoot/Application.java
+++ src/main/java/com/michael/springBoot/Application.java
@@ -4,11 +4,13 @@ import com.michael.springBoot.dao.BaseMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages="com.michael.springBoot.dao",markerInterface = BaseMapper.class)
-@EnableScheduling
+//@EnableScheduling
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
